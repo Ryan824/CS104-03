0b8059e7507a6b31c86ea2c375c5f4f61515fc0a
Ryan824 <71734108+Ryan824@users.noreply.github.com>
2020-11-24 16:51:00 -0500

    Python Queue

--- Python Queue.py
+++ Python Queue.py
@@ -1,5 +1,5 @@
-names = ["Jim", "Bill","Abby","Jeff","Sam","Chris","Emily","Rob","Ryan","Eric"]
-for x in range(0,9):
+names = ["Jim","Bill","Abby","Jeff","Sam","Chris","Emily","Rob","Ryan","Eric"]
+for x in range(0,10):
     names.append(0)
     names.append(1)
     names.append(2)
@@ -12,6 +12,6 @@ for x in range(0,9):
     names.append(9)
     names.append(10)
     
-for x in range(0,9):
+for x in range(0,10):
     len(names)
     print(names.pop(0))
