9c34e15eb1bedfe0bdd8f8c5bf82145c0090e7c7
Ryan824 <71734108+Ryan824@users.noreply.github.com>
2020-11-24 12:45:09 -0500

    Python Queue

---
+++ Python Queue.py
@@ -0,0 +1,8 @@
+names = ["Jim", "Bill","Abby","Jeff","Sam","Chris","Emily","Rob","Ryan","Eric"]
+b = input("Enter a number from 0-9: ")
+for b in range(0,10):
+    names.append(b)
+    
+for b in range(0,10):
+    len(names)
+    print(names.pop(b))
